705f0a842974ba0b72a1f9b59baab7aa7c46d9ca
Cody Deckard <cjdeckard@gmail.com>
2018-10-01 19:44:22 +0000

    Adds Default Markup Text
    Adds default markup text so users can see what markup is capable of.
    It can also give them ideas to utilize in the previewer.

--- client/src/Reducers/MarkupInputReducer.js
+++ client/src/Reducers/MarkupInputReducer.js
@@ -1,5 +1,40 @@
 const defaultState = {
-  markupText: '###Enter Text Here to be Displayed Below',
+  markupText: `# Enter Text Here to be Displayed Below
+ ## Some examples of things you can do...
+* You can **bold** text
+* You can create a [a link to google.com](https://www.google.com)
+* You can create inline code \`$ netcat -nc \`
+* You can create a code block:
+\`\`\`cpp
+class Solution {
+  public:
+    double myPower(double x, int n) {
+      if (x== 1 || x == 0) return x;
+      unsigned absn = abs(n);
+      if (n==0) return 1;
+      if (n==1) return x;
+      
+      if(absn & 1) {
+        return n < 0 ? 1 / (x * power(x * x, absn >> 1)) : x * power(x * x, absn >> 1);
+        
+      }
+      return n<0 > 1/ (power(x*x, absn >> 1)) : power(x*x, absn >> 1);
+    }
+  double power(double x, unsigned n) {
+    if(n == 0) return 1;
+    if(n == 1) return x;
+    if(n & 1) return x * power( x * x, n >>1 );
+    
+    return power(x * x, n >> 1);
+  }
+};
+\`\`\`
+
+> You can use blockquotes to separate
+> your text from other text
+
+![You can even have a logo](https://www.dropbox.com/s/oo4v40hudfw0vx4/map-icon.png?raw=1 "Logo Text")
+`,
 }
 export default (state = defaultState, action) => {
   switch (action.type) {
