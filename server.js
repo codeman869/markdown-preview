355b579529f9dd8e929ed988dcdf8c3b4a8b22d0
Cody Deckard <cjdeckard@gmail.com>
2018-10-02 15:16:25 +0000

    Deployment Scripts for Heroku

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 /* eslint-env node */
 const express = require('express')
 const path = require('path')
+const helmet = require('helmet')
 
 const app = express()
+app.use(helmet())
 
 app.use(express.static(path.join(__dirname, 'client', 'build')))
 
